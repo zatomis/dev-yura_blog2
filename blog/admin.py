011c6ea671d19a6347756275e68543c0d36ba077
zatomis <zatomis@list.ru>
2023-10-13 13:04:56 +0300

    Оптимизация админ панели

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,22 @@ from django.contrib import admin
 from blog.models import Post, Tag, Comment
 
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    raw_id_fields = [
+        'author',
+        'tags',
+        'likes',
+    ]
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['post','text']
+    raw_id_fields = [
+        'post',
+        'author'
+    ]
+
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Tag)
-admin.site.register(Comment)
+admin.site.register(Comment, CommentAdmin)
